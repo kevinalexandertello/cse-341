80d7b43e2ebbcea87cdf0108c17d10bebbe72558
kevinalexandertello <gaston_alexander@hotmail.es>
2021-09-27 20:11:35 -0300

    yes

--- week2/app.js
+++ week2/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
+const books = [];
 app.set('view engine','pug');
 app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false }));
 
 app.get('/' , (req , res, next) => {
     res.render('index' , {pageTitle: 'Add-Book'});
 });
 
 app.get('/books' , (req, res , next) => {
-    res.render('books' , {pageTitle: "Book"});
+    res.render('books' , {pageTitle: "Book",books: books});
 });
 
 app.post('add-book', (req, res , next) => {
+        books.push({name: req.body.bookname});
     res.redirect('/books');
 });
 
