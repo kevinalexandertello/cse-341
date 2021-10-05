968f2208ce8cfb82809fc6146f2b012a110485b9
kevinalexandertello <gaston_alexander@hotmail.es>
2021-10-04 22:46:57 -0300

    yes

--- week2/app.js
+++ week2/app.js
@@ -4,7 +4,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-const users = [];
+const books = [];
 
 app.engine('hbs', expressHbs({ defaultLayout: 'main-layout', extname: 'hbs' }));
 app.set('view engine', 'ejs');
@@ -13,20 +13,20 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
-  res.render('index', { pageTitle: 'Add User' });
+  res.render('index', { pageTitle: 'Add Book' });
 });
 
-app.get('/users', (req, res, next) => {
-  res.render('users', {
-    pageTitle: 'User',
-    users: users,
-    hasUsers: users.length > 0
+app.get('/books', (req, res, next) => {
+  res.render('books', {
+    pageTitle: 'Books',
+    books: books,
+    hasBooks: books.length > 0
   });
 });
 
-app.post('/add-user', (req, res, next) => {
-  users.push({ name: req.body.username });
-  res.redirect('/users');
+app.post('/add-book', (req, res, next) => {
+  users.push({ name: req.body.bookname });
+  res.redirect('/books');
 });
 
 app.listen(3000);
